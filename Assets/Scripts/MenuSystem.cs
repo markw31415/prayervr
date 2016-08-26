93d42817300ea49cad628a898ce40cabfe9654ca
corpusc <corpus_callosum@hotmail.com>
2016-08-26 02:17:42 -0400

    + initial visuals for menu.  look straight up

---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class MenuSystem : MonoBehaviour {
+	Transform self; // cache reference to our own transform, 
+		// so it doesn't constantly query for component per frame (under the hood)
+	Transform host; // the object we attach to and move around with 
+		// (keeping our own seperate rotation on 2 axes) 
+	MenuMode mode;
+
+	enum MenuMode {
+		Compact,
+		Main,
+		Controls,
+		Music,
+		Sound
+	}
+
+
+
+	void Start() {
+		self = transform;
+		host = GameObject.Find("DevSpectate").transform;
+	}
+	
+
+	void LateUpdate() {
+		self.position = host.position;
+		self.eulerAngles = new Vector3(-90f, host.eulerAngles.y, 0f);
+	}
+
+
+	public void GoToMainMenu() {
+		mode = MenuMode.Main;
+		// TODO: disable start/mainmenu button
+		// TODO: build and enable mainmenu buttons
+	}
+}
