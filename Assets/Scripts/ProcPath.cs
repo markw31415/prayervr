55af0e713660e742e9a89d129f5c50ed29bc7670
corpusc <corpus_callosum@hotmail.com>
2016-09-15 17:48:10 -0400

    plotting the points of 1 edge of new path curves

--- Assets/Scripts/ProcPath.cs
+++ Assets/Scripts/ProcPath.cs
@@ -4,22 +4,18 @@ using System.Collections.Generic;
 
 
 public class ProcPath : MonoBehaviour {
-	public List<Vector3> newVertices = new List<Vector3>();
-	public List<Vector2> newUV = new List<Vector2>();
-	public List<int> newTriangles = new List<int>();
+	public List<Vector3> verts = new List<Vector3>();
+	public List<Vector2> uvs = new List<Vector2>();
+	public List<int> tris = new List<int>();
 
 	// private 
 	float rad = 2048f; // radius of entire maze 
 	float wid = 3f; // path width 
-	float minLen = 0.5f; // minimum line length (resolution of curves) 
-	//			perhaps an angular resolution would make a lot more sense and make calculations more predictable 
 	//Vector3 currPos = new Vector3(385f, 1.5f, 447f);
-	//Quaternion currRot;
-
 	Transform tr;
+	Mesh m;
 	MeshFilter mf;
 	MeshRenderer mr;
-	Mesh mesh = new Mesh();
 
 
 
@@ -28,16 +24,42 @@ public class ProcPath : MonoBehaviour {
 		mf = gameObject.AddComponent<MeshFilter>();
 		mr = gameObject.AddComponent<MeshRenderer>();
 
-		mf.mesh = mesh;
-		mesh.vertices = newVertices.ToArray();
-		mesh.uv = newUV.ToArray();
-		mesh.triangles = newTriangles.ToArray();
+		m = new Mesh();
+		mf.mesh = m;
 
-		makeElbow(-45f, new Vector3(-2f, 0f, 2f));
+		m.vertices = verts.ToArray();
+		m.uv = uvs.ToArray();
+		m.triangles = tris.ToArray();
+
+		makeArcTo(new Vector3(11f, 0f, -4f), 10, -1.5f);
 	}
 
 
-	void makeElbow(float degreeDelta, Vector3 posDelta) { // the difference in degrees and position at the end 
-		
+	// lateralDist to the center of a circle.  which is the distance away (laterally, considering
+	// the "movement" direction) from the center point (which lies between our current pos and the endpoint target pos). 
+	// in other words, it is a single float value that gives our distance to the left (neg) or right (pos), where we place
+	// the center of the circle (a segment of the perimeter of this circle forms the arc) 
+	void makeArcTo(Vector3 posDelta, int numPoints, float lateralDist) {
+		float curr = 0f; // current point in 0f - 1f spectrum 
+		float inc = 1f / numPoints; // increment interval through spectrum
+
+		// show center point 
+		tr.forward = posDelta;
+		var pivotAnchor = tr.position + posDelta/2 + tr.right*lateralDist;
+		makePoint(pivotAnchor, Color.red);
+		var beg = tr.position - pivotAnchor; // beginning direction  
+		var end = (tr.position + posDelta) - pivotAnchor; // end direction 
+
+		for (int i = 0; i <= numPoints; i++) {
+			makePoint(pivotAnchor + Vector3.Slerp(beg, end, curr), Color.black);
+			curr += inc;
+		}
+	}
+
+
+	void makePoint(Vector3 v, Color c) {
+		var o = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		o.transform.position = v;
+		o.GetComponent<Renderer>().material.color = c;
 	}
 }
